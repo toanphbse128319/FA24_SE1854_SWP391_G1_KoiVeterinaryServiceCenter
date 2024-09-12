d3f6ba7a5b71cbd1e380dcc48daee03224245e18
quanvo <quanvmse182416@fpt.edu.vn>
2024-09-12 11:13:10 +0700

    Add new tables

--- Source code/KoiVet.sql
+++ Source code/KoiVet.sql
@@ -34,17 +34,67 @@ Create table Customer(
 )
 
 Go
-create table Appointment_Type(
-TypeID varchar(20) primary key, 
-Name nvarchar(50),
-isActive bit
+
+
+
+
+create table Veterinarian(
+VeterinarianID varchar(20) primary key,
+Email nvarchar unique not null,
+RoleID varchar(20) foreign key references Role(RoleID),
+PhoneNumber varchar(10) unique not null,
+Firstname nvarchar(20),
+Lastname nvarchar(20),
+Sex int ,
+Birthday date,
+Avatar varchar,
+Address nvarchar(100),
+Password varchar(50),
+Status bit
 )
 
-Go
+go
+Create table Payment(
+PaymentID varchar(20) primary key,
+PaymentMethod varchar(50)
+)
+go
 
+create table ServicesCategories(
+CategoryID varchar(20) primary key,
+Name varchar(100)
+)
+
+go
+create table Service(
+ServiceID varchar(20) primary key,
+CategoryID varchar(20) foreign key references ServicesCategories(CategoryID),
+Thumbnail varchar,
+Name nvarchar(30),
+Price money,
+Description nvarchar
+)
+go
+
+create table TimeSlot(
+VeterinarianID varchar(20) foreign key references Veterinarian(VeterinarianID),
+Date date,
+SlotID int primary key,
+Status bit ,
+Note varchar(100)
+)
+
+
+go
 create table Appointment(
 AppointmentID varchar(20),
 CustomerID varchar(20) foreign key references Customer(CustomerID),
-TypeID varchar(20) foreign key  references Appointment_Type(TypeID),
-
+VeterinarianID varchar(20) foreign key references Veterinarian(VeterinarianID),
+AppointmentDate date,
+ExpiredDate date,
+TotalPrice money,
+PaymentID varchar(20) foreign key references Payment(PaymentID),
+ServiceID varchar(20) foreign key references Service(ServiceID),
+SlotID int foreign key references TimeSlot(SlotID), 
+Status int 
 )
