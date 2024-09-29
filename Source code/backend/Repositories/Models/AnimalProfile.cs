2aaf812813867e1662314efeb784c41c1f4360ae
quanvo <quanvmse182416@fpt.edu.vn>
2024-09-29 16:32:59 +0700

    Added AnimalProfile model

---
+++ Source code/backend/Repositories/Models/AnimalProfile.cs
@@ -0,0 +1,49 @@
+﻿namespace Repositories.Models;
+
+public class AnimalProfile
+{
+    private String _animalProfileID;
+    private String _name;
+    private String _typeID;
+    private String _bookingDetailID;
+    private String _size;
+    private String _age;
+    private String _color;
+    private String? _description;
+    private bool _sex;
+    private String? _picture;
+
+    public AnimalProfile(string AnimalProfileID, string Name, string TypeID, string BookingDetailID, string Size, string Age, string Color, string? Description, bool Sex, string? Picture)
+    {
+        _animalProfileID = AnimalProfileID;
+        _name = Name;
+        _typeID = TypeID;
+        _bookingDetailID = BookingDetailID;
+        _size = Size;
+        _age = Age;
+        _color = Color;
+        _description = Description;
+        _sex = Sex;
+        _picture = Picture;
+    }
+
+    public string AnimalProfileID { get; set; }
+
+    public string Name { get; set; }
+
+    public string TypeID { get; set; }
+
+    public string BookingDetailID { get; set; }
+
+    public double Size { get; set; }
+
+    public int Age { get; set; }
+
+    public string Color { get; set; }
+
+    public string Description { get; set; }
+
+    public int Sex { get; set; }
+
+    public string Picture { get; set; }
+}
