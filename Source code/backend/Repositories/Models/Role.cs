fe6952573fd77329144838a64c93713609e252e0
quanvo <quanvmse182416@fpt.edu.vn>
2024-09-29 15:58:10 +0700

    Added Role model

---
+++ Source code/backend/Repositories/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿namespace Repositories.Models;
+
+public class Role
+{
+    private string _roleID;
+
+    private string _roleName;
+
+    public Role(string RoleID, string RoleName)
+    {
+        _roleID = RoleID;
+        _roleName = RoleName;
+    }
+
+    public string RoleId { get; set; }
+
+    public string RoleName { get; set; }
+}
