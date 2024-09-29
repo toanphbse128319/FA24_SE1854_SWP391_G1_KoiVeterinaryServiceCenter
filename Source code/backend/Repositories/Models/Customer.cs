44072ff344233767da077e241df57d993081f0d8
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-09-29 14:46:35 +0700

    Add Customer model

--- Source code/backend/Repositories/Models/Customer.cs
+++ Source code/backend/Repositories/Models/Customer.cs
@@ -1,60 +1,45 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Repositories.Models;
 
 public class Customer{
     private string _id;
     private string _email;
-    private string _roleId;
     private string _phoneNumber;
     private string? _firstName;
     private string? _lastName;
-//  private int _sex;
+    private bool _sex;
     private DateOnly _birthday;
-//  private string _avatar;
+    private string _avatar;
     private string _address;
-    private string _password;
-//  private string _status;
-//  private string _picture;
+    private string _accountID;
+    private string _status;
 
-    public string Id {
-        get { return _id; }
-        set { _id = value; }
+    public Customer(string ID, string Email, string PhoneNumber, string FirstName, string LastName, bool sex, DateOnly BirthDay, string Avatar, string Address, string AccountID, string Status ){
+        _id = ID;
+        _email = Email;
+        _phoneNumber = PhoneNumber;
+        _firstName = FirstName;
+        _lastName = LastName;
+        _sex = sex;
+        _birthday = BirthDay;
+        _avatar = Avatar;
+        _address = Address;
+        _accountID = AccountID;
+        _status = Status;
     }
 
-    public string Email {
-        get { return _email; }
-        set { _email = value; }
-    }
-    
-    public string RoleId {
-        get { return _roleId; }
-        set { _roleId = value; }
-    }
+    public string Id { get; set; }
 
-    public string PhoneNumber {
-        get { return _phoneNumber; }
-        set { _phoneNumber = value; }
-    }
-    
-//    public string? FirstName {
-//        get { return _firstName; }
-//        set { _firstName = value; }
-//    }
+    public string Email { get; set; }
 
-    public DateOnly BirthDay {
-        get { return _birthday; }
-        set { _birthday = value; }
-    }
+    public string Lastname { get; set; }
 
-    public string Address {
-        get { return _address; }
-        set { _address = value; }
-    }
+    public string FirstName { get; set; }
 
-    public string Password {
-        get { return _password; }
-        set { _password = value; }
-    }
+    public string Avatar { get; set; }
 
+    public string Address { get; set; }
+
+    public string AccountID { get; set; }
+
+    public string Status { get; set; }
 }
