dcfc12f5d21744e15b7498bf6f388c2be36f46ed
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-09-30 23:37:20 +0700

    Added AccountDAO which contain method to login using email and password

---
+++ Source code/backend/Business/DAO/AccountDAO.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using KVCS.Context;
+using KVCS.Model;
+
+namespace KVCS.Business;
+public class AccountDAO {
+    private readonly AccountContext _context = null!;
+
+    public AccountDAO(AccountContext context){
+        if ( context is null ) {
+            throw new Exception("Context is not initialized");
+        }
+        _context = context;
+
+    }
+
+    public async Task<Account> LoginByEmail( string email, string password ){
+        return await _context.Accounts.FromSql($"SELECT * FROM Account WHERE email = {email} AND password = {password}").FirstOrDefaultAsync();
+    }
+}
