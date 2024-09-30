58a2103e136d7cb2cb1023a914e033bfa5e3184b
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-09-30 11:57:33 +0700

    Add account model

--- Source code/backend/Repositories/Models/Account.cs
+++ Source code/backend/Repositories/Models/Account.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel.DataAnnotations.Schema;
 namespace KVCS.Model;
 
+// Tên table từ dababase ghi ở đây
 [Table("Account")]
 public class Account{
 
     public string AccountID { get; set; }
     public string Email { get; set; }
+    public string PhoneNumber { get; set; }
     public string RoleID { get; set; }
     public string Avatar { get; set; }
     public string Password { get; set; }
