713df441d8ae03613a0853610023b74e32e49522
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-10-01 12:31:10 +0700

    Create Booking context

---
+++ Source code/backend/Repositories/Data/BookingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Models;
+
+namespace Repositories.Data
+{
+    public class BookingContext : DbContext 
+    {
+        public BookingContext(DbContextOptions<BookingContext> options)
+        : base(options)
+        {
+        }
+        DbSet<Booking> Bookings { get; set; }
+    }
+}
