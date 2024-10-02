845630d1da4d8ebe603af586ccb081685f4b5074
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-03 00:40:51 +0700

    Added method for LoginInformation to check if null or empty

--- Source code/backend/Helper/LoginInfomation.cs
+++ Source code/backend/Helper/LoginInfomation.cs
@@ -4,4 +4,12 @@ namespace Helper;
 public class LoginInformation{
     public string Info { get; set; }
     public string Password { get; set; }
+
+    public bool IsEmpty(){
+        if ( Info == null || Password == null )
+            return true;
+        if ( Info.Trim() == "" || Password.Trim() == "" )
+            return true;
+        return false;
+    }
 }
