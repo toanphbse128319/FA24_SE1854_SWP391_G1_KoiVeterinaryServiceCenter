a0d745925d077c63483a27e4a50c3d2715c4a834
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-04 11:15:45 +0700

    Added FAQ repository to unit of work

--- Source code/backend/Repositories/UnitOfWork.cs
+++ Source code/backend/Repositories/UnitOfWork.cs
@@ -4,6 +4,7 @@ namespace Repositories;
 
 public class UnitOfWork{
     private Context _context;
+    private FAQRepository _faqRepository;
     private AccountRepository _accountRepository;
 
     public UnitOfWork(){
@@ -12,5 +13,9 @@ public class UnitOfWork{
 
     public AccountRepository AccountRepository {
         get { return _accountRepository ??= new AccountRepository(_context); }
+    }
+    
+    public FAQRepository FAQRepository {
+        get { return _faqRepository ??= new FAQRepository(_context); }
     }
 }
