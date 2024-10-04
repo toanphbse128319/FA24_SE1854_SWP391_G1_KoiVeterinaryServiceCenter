a121cd51e094bcc9b217d27ae3de657df2db0981
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-04 11:25:15 +0700

    Added API endpoint to get all FAQ

---
+++ Source code/backend/API/Controller/FAQController.cs
@@ -0,0 +1,26 @@
+#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Model;
+using Repositories;
+
+namespace API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FAQController : ControllerBase
+    {
+        private UnitOfWork _unitOfWork;
+        public FAQController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FAQ>>> GetAll(){
+            return await _unitOfWork.FAQRepository.GetAllAsync();
+
+        }
+
+    }
+}
