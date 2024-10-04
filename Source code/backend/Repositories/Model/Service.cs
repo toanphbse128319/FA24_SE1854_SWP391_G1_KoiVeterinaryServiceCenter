5c06b5bec9d07cf1a709c286acda2f6367c621a8
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-04 13:43:41 +0700

    Change Data type of price from Int to Decimal

--- Source code/backend/Repositories/Model/Service.cs
+++ Source code/backend/Repositories/Model/Service.cs
@@ -7,7 +7,7 @@ public class Service {
     public string ServiceID { get; set; }
     public string ServiceDeliveryMethodID { get; set; }
     public string Name { get; set; }
-    public int Price { get; set; }
+    public Decimal Price { get; set; }
     public string Description { get; set; }
     public string Status { get; set; }
 
