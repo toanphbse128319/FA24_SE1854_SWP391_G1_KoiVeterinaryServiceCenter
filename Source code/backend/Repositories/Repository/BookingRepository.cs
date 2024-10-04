7c253cfe990f37ef43c9c070c82b49fa4b418b6e
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-10-04 16:17:43 +0700

    Added Booking Repository

---
+++ Source code/backend/Repositories/Repository/BookingRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Data;
+using Repositories.Model;
+
+namespace Repositories.Repository
+{
+    public class BookingRepository : GenericRepository<Booking>
+    {
+
+        public BookingRepository(Context context)
+        {
+            
+            _context = context;
+        }
+        public Task<Booking?> FindBookingAsync(string id)
+        {
+            return _context.Bookings.FirstOrDefaultAsync(booking => booking.BookingID == id);
+        }
+        public async Task<Booking?> UpdateStatusAsync(string id, string msg)
+        {
+            Booking booking = await FindBookingAsync(id);
+            if (booking == null)
+            {
+                return booking;
+            }
+            else
+            {
+                booking.Status = msg;
+                _context.SaveChangesAsync();
+                return booking; 
+            }
+        }
+
+        
+    }
+}
