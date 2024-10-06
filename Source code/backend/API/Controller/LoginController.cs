25bd1fefc6139178837688b03b6922220d84dfe1
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-06 19:03:51 +0700

    Added some condition before checking login information

--- Source code/backend/API/Controller/LoginController.cs
+++ Source code/backend/API/Controller/LoginController.cs
@@ -19,6 +19,16 @@ public class LoginController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Account>> LoggingByEmail(LoginInformation info){
         try{
+
+            if( info.Info == null )
+                return BadRequest("Missing parameter: Info");
+            if( info.Password == null )
+                return BadRequest("Missing parameter: password");
+            if( info.Info.Trim() == "" )
+                return BadRequest("Info cannot be empty");
+            if( info.Password.Trim() == "" )
+                return BadRequest("Password cannot be empty");
+
             var account = await _unitOfWork.AccountRepository.LoginAsync( info );
             if( account == null )
                 return StatusCode(StatusCodes.Status401Unauthorized, "Invalid username or password!");
