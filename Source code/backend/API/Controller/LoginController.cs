a21491777e8724d2be82309cfddeb879255b3dd7
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-07 16:49:03 +0700

    Return authorization token after a success login with a lifetime of 3 hours

--- Source code/backend/API/Controller/LoginController.cs
+++ Source code/backend/API/Controller/LoginController.cs
@@ -1,4 +1,9 @@
 #nullable disable
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
 using Microsoft.AspNetCore.Mvc;
 using Repositories.Model;
 using Repositories;
@@ -11,9 +16,12 @@ namespace API.Controllers;
 public class LoginController : ControllerBase
 {
     private UnitOfWork _unitOfWork;
+    private IConfiguration _configuration;
+
     public LoginController(UnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
+        _configuration = Configuration.GetConfiguration();        
     }
 
     [HttpPost]
@@ -32,9 +40,40 @@ public class LoginController : ControllerBase
             var account = await _unitOfWork.AccountRepository.LoginAsync( info );
             if( account == null )
                 return StatusCode(StatusCodes.Status401Unauthorized, "Invalid username or password!");
-            account.Password = "******";
-            return account!;
+
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"] ?? string.Empty);
+            var claims = new List<Claim>{
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, account.Email),
+                //new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())  
+                /*
+                 * The identifier value MUST be assigned in a manner that ensures that
+                 *there is a negligible probability that the same value will be
+                 *accidentally assigned to a different data object; if the application
+                 *uses multiple issuers, collisions MUST be prevented among values
+                 *produced by different issuers as well. The "jti" claim can be used
+                 *to prevent the JWT from being replayed. The "jti" value is a case-
+                 *sensitive string. Use of this claim is OPTIONAL.
+                 */
+            }; 
+
+            claims.Add(new Claim(ClaimTypes.Role, _unitOfWork.RoleRepository.getRoleName(account.RoleID)));
+            
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(4),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials( new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var stringToken = tokenHandler.WriteToken(token);
+            return Ok(stringToken);
         } catch ( Exception ex ){
+            Console.WriteLine(ex);
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
