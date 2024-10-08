978f0061f9801f5dc83ade439914b3809f28a938
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-10-08 15:37:43 +0700

    Added Employee repository and FindEmpById method

---
+++ Source code/backend/Repositories/Repository/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Model;
+
+namespace Repositories.Repository
+{
+    public class EmployeeRepository : GenericRepository<AnimalType>
+    {
+        public EmployeeRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public Task<Employee?> FindEmpByIdAsync(string id)
+        {
+            return _context.Employees.FirstOrDefaultAsync(employee => employee.EmployeeID == id)!;
+        }
+    }
+}
