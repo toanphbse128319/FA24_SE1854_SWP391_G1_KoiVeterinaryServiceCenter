e5ae4f889d88e0a5abb671fd5ce42efa8d59cdbe
tawng <longtnhse173174@fpt.edu.vn>
2024-10-10 00:41:37 +0700

    add Schedule model

---
+++ Source code/backend/Repositories/Model/Schedule.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Repositories.Model;
+
+[Table("Schedule")]
+public class Schedule
+{   
+    public string ScheduleID { get; set; }
+    public string EmployeeID { get; set; }
+    public string Date { get; set; }
+    public string Note { get; set; }
+    public string Slot { get; set; }
+    public string SlotCapacity { get; set; }
+    public string SlotStatus { get; set; }
+
+}
+
