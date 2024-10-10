423a36f5a3c4edba789d385e97164fd1a1355021
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-10 16:35:17 +0700

    check branch - Restored version

---
+++ Source code/backend/Repositories/Repository/ScheduleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using Repositories.Model;
+using Repositories;
+using Repositories.Repository;
+
+namespace Repositories.Repository;
+
+public class ScheduleRepository : GenericRepository<Schedule>
+{
+    public ScheduleRepository(Context context)
+    {
+        _context = context;
+    }
+}
+
