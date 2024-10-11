971a753f06da4b120549d8edcc327c866a5c8a7f
kietKS <kietnase185061@fpt.edu.vn>
2024-10-11 22:20:08 +0700

    them feedbackCOntroller

---
+++ Source code/backend/API/Controller/FeedbackController.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Repositories.Model;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FeedbackController : ControllerBase
+{
+    private UnitOfWork _unitOfWork;
+    public FeedbackController(UnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Feedback>>> GetAll()
+    {
+        return await _unitOfWork.FeedbackRepository.GetAllAsync();
+
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Feedback>> GetFeedbackByID(string id)
+    {
+        return await _unitOfWork.FeedbackRepository.GetByIdAsync(id);
+    }
+
+    [HttpPut]
+    public ActionResult<Feedback> SaveFeedback(Feedback feedback)
+    {
+        try {
+            feedback.FeedbackID = null;
+            _unitOfWork.FeedbackRepository.SaveFeedBack(feedback);
+            return  Ok("them feedback thanh cong");
+        }
+        catch(Exception ex)
+        {   Console.WriteLine(ex.ToString());
+            return StatusCode (StatusCodes.Status500InternalServerError,ex.Message);
+        }
+
+    }
+}
