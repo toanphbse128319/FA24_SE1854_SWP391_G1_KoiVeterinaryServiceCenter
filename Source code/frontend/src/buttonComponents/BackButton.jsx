a63fe4115f6b96b16aac7289baa7cccebaf302df
kietKS <kietnase185061@fpt.edu.vn>
2024-10-11 21:20:17 +0700

    them component nut tro ve

--- Source code/frontend/src/buttonComponents/BackButton.jsx
+++ Source code/frontend/src/buttonComponents/BackButton.jsx
@@ -6,12 +6,23 @@ const BackButton = () => {
   const navigate = useNavigate();
 
   const handleBack = () => {
-    navigate(-1); // Quay lại trang trước trong lịch sử
+    navigate(-1);
   };
 
   return (
-    <button onClick={handleBack}>
-      Quay Lại
+    <button
+      onClick={handleBack}
+      style={{
+        position: 'fixed',    // Định vị cố định
+        top: '90px',          // Cách từ đỉnh 20px
+        left: '50px',         // Cách từ bên trái 20px
+        border: 'none',       // Xóa viền nút
+        background: 'none',   // Xóa nền nút
+        cursor: 'pointer',    // Chỉ thị con trỏ chuột khi hover
+        zIndex: 1000          // Đảm bảo nút luôn nằm trên các phần tử khác
+      }}
+    >
+      <img src='\img\TrangChu\backButton.png' alt="Back" />
     </button>
   );
 };
