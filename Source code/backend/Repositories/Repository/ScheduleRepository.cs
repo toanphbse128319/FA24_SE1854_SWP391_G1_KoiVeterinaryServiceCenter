00b4d8cf704360e50a562d76676da7f741e86ffd
Tawng <truongnguyenhoanglong@gmail.com>
2024-10-12 02:30:24 +0700

    add Schedule Repo

--- Source code/backend/Repositories/Repository/ScheduleRepository.cs
+++ Source code/backend/Repositories/Repository/ScheduleRepository.cs
@@ -12,5 +12,32 @@ public class ScheduleRepository : GenericRepository<Schedule>
     {
         _context = context;
     }
+    public Task<Schedule?> FindScheduleAsync(string id)
+    {
+        return _context.Schedules.FirstOrDefaultAsync(schedule => schedule.ScheduleID == id);
+    }
+
+    public Task<List<Schedule>> GetScheduleAsync(string date)
+    {
+        return _context.Schedules.Where(schedule => schedule.Date == date).ToListAsync();
+    }
+    public Task<List<Schedule>> GetSlotAvailableAsync(string date) 
+    { 
+        return _context.Schedules.Where(schedule => schedule.Date == date &&
+                                                    schedule.SlotStatus == "true").ToListAsync();
+    }
+
+    public async Task<Schedule?> UpdateSlotStatusAsync(string id, string msg)
+    {
+        Schedule schedule = await FindScheduleAsync(id);
+        if (schedule == null) 
+            return schedule;
+        else
+        {
+            schedule.SlotStatus = msg;
+            await _context.SaveChangesAsync();
+            return schedule;
+        }
+    }
 }
 
