4806d5c7c009ffdbef03814cbf3f89064211ab3f
kietKS <kietnase185061@fpt.edu.vn>
2024-10-12 10:22:45 +0700

    nut aboutus

--- Source code/frontend/src/buttonComponents/AboutUs.jsx
+++ Source code/frontend/src/buttonComponents/AboutUs.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Login from '../pages/Login';
+import Button from '@mui/material/Button';
+import { styled } from '@emotion/styled';
 
-const AboutUs = () => {
+const StyledButton = styled(Button)`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  color: #64B0E0;
+  font-size: 24px;
+  font-family: 'Inter', sans-serif;
+  font-weight: 600;
+  letter-spacing: 0.24px;
+  word-wrap: break-word;
+`;
+
+const AboutButton = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <Link to="Login">
-        <button>AboutUs</button>
-      </Link>
-    </div>
+    <StyledButton variant="outlined">
+      About us
+    </StyledButton>
   );
 };
 
-export default AboutUs;
-
-
+export default AboutButton;
