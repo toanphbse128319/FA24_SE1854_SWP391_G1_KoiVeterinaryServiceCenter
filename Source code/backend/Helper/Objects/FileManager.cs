0f6025d8a675c45a1042295ff39848ac38a6fd41
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-13 16:52:16 +0700

    Added object to read file locally (May not work with window)

---
+++ Source code/backend/Helper/Objects/FileManager.cs
@@ -0,0 +1,24 @@
+#nullable disable
+
+namespace Helper;
+
+public class FileManager{
+    private string _filePath;
+
+    public FileManager(string relativePath){
+        _filePath = Directory.GetCurrentDirectory() + relativePath;
+        if( File.Exists(_filePath) == false )
+            throw new FileNotFoundException("Directory: " + _filePath);
+
+    }
+
+    public string ReadFile(){
+        StreamReader fileReader = File.OpenText(_filePath);
+        string result = null;
+        string sub;
+        while( (sub = fileReader.ReadLine()) != null )
+            result += (sub + '\n');
+
+        return result;
+    }
+}
