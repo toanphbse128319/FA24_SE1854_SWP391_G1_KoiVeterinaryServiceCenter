15dfce6012406e8efd450eda7938ef18ed029732
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-13 16:52:56 +0700

    Added some infomation for email and otp waiting state

--- Source code/backend/Helper/Constants.cs
+++ Source code/backend/Helper/Constants.cs
@@ -4,9 +4,21 @@ public static class Constants{
 
     public static class Mail{
         private static string _emailSendFailedMessage = "Unable to send mail";
-
+        private static string _signUpEmailLocation = "/../Helper/Asset/signupotpemail";
         public static string EmailSendFailedMessage{
             get => _emailSendFailedMessage;
+        }
+
+        public static string signUpEmailLocation{
+            get => _signUpEmailLocation;
+        }
+    }
+
+    public static class Account{
+        private static string _waitingForOTPMessage = "Waiting for OTP: ";
+
+        public static string WaitingForOTPMessage{
+            get => _waitingForOTPMessage;
         }
     }
 
