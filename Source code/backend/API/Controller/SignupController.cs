7797de6ceba9578a5959ab21b32485f56645fef9
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-13 17:01:13 +0700

    Added new endpoint to check the OTP code

--- Source code/backend/API/Controller/SignupController.cs
+++ Source code/backend/API/Controller/SignupController.cs
@@ -40,15 +40,42 @@ namespace API.Controllers
                 if( _unitOfWork.AccountRepository.FindPhoneNumber(info.PhoneNumber) != null )
                     return Conflict("That phone number is already used!");
 
-                var account = await _unitOfWork.AccountRepository.SignUpAsync( info );
-                if( account.Status == Constants.Mail.EmailSendFailedMessage )
+                string result = await _unitOfWork.AccountRepository.CustomerSignUpAsync( info );
+                if( result == "Unable to send mail")
                     return StatusCode(StatusCodes.Status201Created, Constants.Mail.EmailSendFailedMessage);
 
+                Console.WriteLine(info.AccountID);
                 return StatusCode(StatusCodes.Status201Created, "Signup successfully");
             } catch (Exception ex){
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
 
+        [Route("otp")]
+        [HttpPost]
+        public async Task<ActionResult<string>> OTPVerify(LoginInformation info){
+            try{
+                string result = await _unitOfWork.AccountRepository.CheckOtp(info);
+                
+                switch( result ){
+                    case "Cannot find account with that ID": 
+                        return NotFound("Cannot find account with that info");
+                    case "No Need for OTP":
+                        return StatusCode(StatusCodes.Status406NotAcceptable, "Account does not need otp verification");
+                    case "OTP password has expired":
+                        return StatusCode(StatusCodes.Status406NotAcceptable, "OTP code expired");
+                    case "Wrong OTP":
+                        return StatusCode(StatusCodes.Status406NotAcceptable, "OTP code invalid");
+                    case "OTP verify successfully":
+                        return StatusCode(StatusCodes.Status200OK, "OTP verification successfully");
+                    default:
+                        return StatusCode(StatusCodes.Status501NotImplemented, "This should not show up!");
+                }
+
+            } catch (Exception ex){
+                Console.WriteLine(ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
     }
 }
