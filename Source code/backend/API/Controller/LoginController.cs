dd8bf50438e238832b0913725f067b4918460586
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-13 18:38:02 +0700

    Remove logic in API and change the controller execution type to message-based

--- Source code/backend/API/Controller/LoginController.cs
+++ Source code/backend/API/Controller/LoginController.cs
@@ -1,9 +1,4 @@
 #nullable disable
-using System.Text;
-using System.Security.Claims;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
-
 using Microsoft.AspNetCore.Mvc;
 using Repositories.Model;
 using Repositories;
@@ -25,66 +20,22 @@ public class LoginController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Account>> LoggingByEmail(LoginInformation info){
         try{
+            var result = await _unitOfWork.AccountRepository.LoginAsync( info );
 
-            if( info.Info == null )
-                return BadRequest("Missing parameter: Info");
-            if( info.Password == null )
-                return BadRequest("Missing parameter: password");
-            if( info.Info.Trim() == "" )
-                return BadRequest("Info cannot be empty");
-            if( info.Password.Trim() == "" )
-                return BadRequest("Password cannot be empty");
-            var account = await _unitOfWork.AccountRepository.LoginAsync( info );
-            if( account == null )
-                return StatusCode(StatusCodes.Status401Unauthorized, "Invalid username or password!");
-
-            if( account.Status.Contains(Constants.Account.WaitingForOTPMessage) )
-                return StatusCode(StatusCodes.Status403Forbidden, "This account hasn't verify otp code");
-
-            if( account.IsActive == false )
-                return StatusCode(StatusCodes.Status403Forbidden, "Account has been disabled");
-
-            string firstname = null;
-            string lastname = null;
-            Customer customer = await _unitOfWork.CustomerRepository.SearchByAccountID(account.AccountID);
-            Employee employee = null;
-            if( customer == null ){
-                employee = await _unitOfWork.EmployeeRepository.SearchByAccountID(account.AccountID);
-                if( employee == null ){
-                    return StatusCode(StatusCodes.Status404NotFound, "Cannot find profile associate with the account");
-                }
-                firstname = employee.FirstName;
-                lastname = employee.Lastname;
-            } else {
-                firstname = customer.FirstName;
-                lastname = customer.Lastname;
-            }
-
-            var currentRole = _unitOfWork.RoleRepository.getRoleName(account.RoleID);
-            
-            Token token = new Token();
-
-            var claims = new List<Claim>{
-                new Claim("Id", Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, account.Email),
-                new Claim(ClaimTypes.Role, currentRole),
-                new Claim("Firstname", firstname),
-                new Claim("Lastname", lastname)
-                //new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())  
-                /*
-                 * The identifier value MUST be assigned in a manner that ensures that
-                 *there is a negligible probability that the same value will be
-                 *accidentally assigned to a different data object; if the application
-                 *uses multiple issuers, collisions MUST be prevented among values
-                 *produced by different issuers as well. The "jti" claim can be used
-                 *to prevent the JWT from being replayed. The "jti" value is a case-
-                 *sensitive string. Use of this claim is OPTIONAL.
-                 */
-            }; 
-            token.Claims = claims;
-
-            return Ok(token.GenerateToken(4));
-
+            switch( result ){
+                case "Info and password cannot be empty":
+                    return BadRequest(result);
+                case "Invalid username or password!":
+                    return StatusCode(StatusCodes.Status401Unauthorized, result);
+                case "This account hasn't verify otp code":
+                    return StatusCode(StatusCodes.Status406NotAcceptable, result);
+                case "Account has been disabled":
+                    return StatusCode(StatusCodes.Status403Forbidden, result);
+                case "Cannot find profile associate with the account":
+                    return StatusCode(StatusCodes.Status417ExpectationFailed, result);
+                default:
+                    return Ok(result);
+            }   
         } catch ( Exception ex ){
             Console.WriteLine(ex);
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
