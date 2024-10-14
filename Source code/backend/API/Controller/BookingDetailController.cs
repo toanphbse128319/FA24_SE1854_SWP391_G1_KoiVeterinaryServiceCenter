24665c2dedfa641a3a997316087e0d8330db68ba
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-10-14 20:54:51 +0700

    Added more get api

--- Source code/backend/API/Controller/BookingDetailController.cs
+++ Source code/backend/API/Controller/BookingDetailController.cs
@@ -18,10 +18,32 @@ namespace API.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("ByBookingDetailID")]
         public async Task<ActionResult<BookingDetail>> GetBookingDetailByID(string id)
         {
-            return await _unitOfWork.BookingDetailRepository.GetByIdAsync(id);
+            BookingDetail bd = await _unitOfWork.BookingDetailRepository.GetByIdAsync(id);
+            if (bd != null)
+                return bd;
+            else
+                return NotFound("Booking Detail does not existed!");
+        }
+
+        [HttpGet("ByBookingID")]
+        public async Task<ActionResult<IEnumerable<BookingDetail?>>> GetBookingDetailsByBookingID(string id)
+        {
+            List<BookingDetail?> list = await _unitOfWork.BookingDetailRepository.GetByBookingID(id);
+            if(list.Count == 0)
+                return NotFound("Booking Detail not found!");
+            else return Ok(list);
+        }
+
+        [HttpGet("ByServiceID")]
+        public async Task<ActionResult<IEnumerable<BookingDetail?>>> GetBookingDetailsByServiceID(string id)
+        {
+            List<BookingDetail?> list = await _unitOfWork.BookingDetailRepository.GetByServiceID(id);
+            if (list.Count == 0)
+                return NotFound("Booking Detail not found!");
+            else return Ok(list);
         }
 
         [HttpPut]
@@ -29,8 +51,21 @@ namespace API.Controllers
         {
             try
             {
-                if(await _unitOfWork.BookingDetailRepository.UpdateAsync(info) == 0)
+                BookingDetail bd = await _unitOfWork.BookingDetailRepository.GetByIdAsync(info.BookingDetailID);
+                if (bd == null)
                     return NotFound("BookingDetail does not existed!");
+                bd.NoteResult = info.NoteResult;
+                bd.DrugList = info.DrugList;
+                bd.MaterialList = info.MaterialList;
+                bd.ConsultDoctor = info.ConsultDoctor;
+                bd.AnimalStatusDescription = info.AnimalStatusDescription;
+                bd.PoolStatusDescription = info.PoolStatusDescription;
+                bd.ConsultTechnician = info.ConsultTechnician;
+                bd.PoolStatusDescription = info.PoolStatusDescription;
+                bd.Incidental = info.Incidental;
+                bd.NoteExamination = info.NoteExamination;
+                if (await _unitOfWork.BookingDetailRepository.UpdateAsync(bd) == 0)
+                    return BadRequest("BookingDetail has not been updated!");
                 else
                     return Ok("Changes have been saved");
             }
@@ -38,8 +73,6 @@ namespace API.Controllers
             {
                 return BadRequest("Unknown Error: " + ex.Message);
             }
-
-
         }
 
     }
