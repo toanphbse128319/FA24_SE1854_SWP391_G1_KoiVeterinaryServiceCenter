ee23f629a9af34f8a424733d3eebe01c004014fa
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-15 10:36:37 +0700

    Changed string type to DateOnly type

--- Source code/backend/Repositories/Model/Schedule.cs
+++ Source code/backend/Repositories/Model/Schedule.cs
@@ -9,7 +9,7 @@ public class Schedule
     public string EmployeeID { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Date { get; set; }
+    public DateOnly Date { get; set; }
     public string Note { get; set; }
     public int Slot { get; set; }
     public int SlotCapacity { get; set; }
