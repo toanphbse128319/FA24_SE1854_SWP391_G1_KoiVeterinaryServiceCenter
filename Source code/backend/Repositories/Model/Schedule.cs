4a597f776ba022c65e245df4ddebeee0181b73a2
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-15 12:15:57 +0700

    Remove Lastname and Firstname because from employeeid because we can get it just from employeeID, which mean this database already violate normalization if left as it is

--- Source code/backend/Repositories/Model/Schedule.cs
+++ Source code/backend/Repositories/Model/Schedule.cs
@@ -7,8 +7,6 @@ public class Schedule
 {   
     public string ScheduleID { get; set; }
     public string EmployeeID { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
     public DateOnly Date { get; set; }
     public string Note { get; set; }
     public int Slot { get; set; }
