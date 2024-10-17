12d520c32523ed3a301e550a934be1413d946eb8
Tawng <truongnguyenhoanglong@gmail.com>
2024-10-17 14:04:12 +0700

    Update schedule model and slottable model

--- Source code/backend/Repositories/Model/Schedule.cs
+++ Source code/backend/Repositories/Model/Schedule.cs
@@ -9,9 +9,6 @@ public class Schedule
     public string EmployeeID { get; set; }
     public DateOnly Date { get; set; }
     public string Note { get; set; }
-    public int Slot { get; set; }
-    public int SlotCapacity { get; set; }
-    public bool SlotStatus { get; set; }
-
+    public string Status { get; set; }
 }
 
