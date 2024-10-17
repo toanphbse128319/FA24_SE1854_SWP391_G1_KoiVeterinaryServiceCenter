fd219e74a8f417893f286e7387e6fad62fb7108f
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-17 23:51:20 +0700

    This is the result of decoupling

--- Source code/backend/API/Controller/SignupController.cs
+++ Source code/backend/API/Controller/SignupController.cs
@@ -18,39 +18,35 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Account>> SignUp(Account info){
-            
-
-
+        public async Task<ActionResult<Account>> SignUp(CustomerSignupInformation info ){
             try{
                 string result = await _unitOfWork.AccountRepository.CustomerSignUpAsync( info );
+                if( result.Contains("cannot be") )
+                    return StatusCode(StatusCodes.Status400BadRequest, result);
                 switch( result ){
-                    case "Missing parameter: Email":
-                    case "Missing parameter: PhoneNumber":
-                    case "Missing parameter: Password":
-                    case "Email must not be empy":
-                    case "PhoneNumber must not be empy":
-                    case "Password must not be empy":
+                    case "Customer is empty":
+                    case "customer name, address cannot be null":
+                    case "Email must not be empty":
+                    case "PhoneNumber must not be empty":
+                    case "Password must not be empty":
                         return BadRequest(result);
-                    case "That email is already usd!":
+                    case "That email is already used!":
                     case "That phone number is alreay used!":
                         return StatusCode(StatusCodes.Status409Conflict, result);
-                    case "Unable to send mail":
-                        return StatusCode(StatusCodes.Status201Created, result);
                     case "Created successfully": 
                         return StatusCode(StatusCodes.Status201Created, "Signup successfully");
                     default: 
                         return StatusCode(StatusCodes.Status501NotImplemented, "You should not be here");
                 }
             } catch (Exception ex){
+                Console.WriteLine(ex);
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
 
-        [Route("otp")]
+        [Route("otp/check")]
         [HttpPost]
         public async Task<ActionResult<string>> OTPVerify(LoginInformation info){
-            
             try{
                 string result = await _unitOfWork.AccountRepository.CheckOtp(info);
                 
@@ -59,7 +55,7 @@ namespace API.Controllers
                         return BadRequest(result);
                     case "Cannot find account with that ID": 
                         return NotFound(result);
-                    case "No Need for OTP":
+                    case "No need for OTP":
                         return StatusCode(StatusCodes.Status406NotAcceptable, result);
                     case "OTP password has expired":
                         return StatusCode(StatusCodes.Status406NotAcceptable, result);
@@ -76,5 +72,32 @@ namespace API.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
+        [Route("otp/send")]
+        [HttpGet]
+        public async Task<ActionResult<string>> SendOTP(string info){
+            try{
+                string result = await _unitOfWork.AccountRepository.SendOtpMail(info);
+                switch ( result ){
+                    case "Info cannot be null!":
+                    case "Info must not be empty!":
+                        return StatusCode(StatusCodes.Status400BadRequest, result);
+                    case "Unable to find the account":
+                        return StatusCode(StatusCodes.Status404NotFound, result);
+                    case "No need for OTP":
+                        return StatusCode(StatusCodes.Status406NotAcceptable, result);
+                    case "Unable to send mail":
+                        return StatusCode(StatusCodes.Status422UnprocessableEntity, result);
+                    case "Send mail successfully":
+                        return StatusCode(StatusCodes.Status200OK, result);
+                    default:
+                        return StatusCode(StatusCodes.Status501NotImplemented, "You should not be here");
+                }
+            } catch (Exception ex){
+                Console.WriteLine(ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }   
     }
 }
