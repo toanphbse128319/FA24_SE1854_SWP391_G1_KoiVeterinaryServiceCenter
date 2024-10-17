e76f2f43d95352bacc2995b48f3f652239bb9728
Tawng <truongnguyenhoanglong@gmail.com>
2024-10-17 13:58:15 +0700

    Add UpdateSchedule to update info about schedule and slottable

---
+++ Source code/backend/Helper/UpdateSchedule.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+namespace Helper;
+
+public class UpdateSchedule
+{
+    public string ScheduleID { get; set; }
+    public string EmployeeID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly Date { get; set; }
+    public int Slot { get; set; }
+    public int SlotCapacity { get; set; }
+    public string SlotStatus { get; set; }
+    public string Note { get; set; }
+}
