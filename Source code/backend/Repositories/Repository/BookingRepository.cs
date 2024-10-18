0ed10df91206b67d9bc0bf2c2cc8bd3b90b931bd
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-18 13:02:53 +0700

    Change input variable of AddBooking from using Booking to NewBookingInformation from Customer.Constants

--- Source code/backend/Repositories/Repository/BookingRepository.cs
+++ Source code/backend/Repositories/Repository/BookingRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Repositories.Model;
+using Helper.Objects;
+using Helper;
 
 namespace Repositories.Repository;
 public class BookingRepository : GenericRepository<Booking>
@@ -34,26 +36,25 @@ public class BookingRepository : GenericRepository<Booking>
      * this will generate Schedule if there's no schedule at the time of booking
      * or will update the slot status to false if the slot is available
      */
-    public async Task<string> SetScheduleAsync(Booking info){
+    public async Task<string> SetScheduleAsync(DateTime bookingDate, string employeeID){
         ScheduleRepository schrepo = new ScheduleRepository(_context);
-        DateOnly date = DateOnly.Parse(info.BookingDate.ToString("yyyy-MM-dd"));
-        Schedule? schedule = await schrepo.CheckValidDateAsync(date, info.EmployeeID);
+        DateOnly date = DateOnly.Parse(bookingDate.ToString("yyyy-MM-dd"));
+        Schedule? schedule = await schrepo.CheckValidDateAsync(date, employeeID);
         if( schedule == null ){
             schedule = new Schedule();
-            schedule.EmployeeID = info.EmployeeID;
+            schedule.EmployeeID = employeeID;
             schedule.Date = date;
             await schrepo.GenerateVetScheduleAsync(schedule);
         }
-        schedule.Slot = schrepo.SlotByTime(info.BookingDate);
+        schedule.Slot = schrepo.SlotByTime(bookingDate);
         if( schedule.Slot == 0 )
             return "Outside working hour";
-        schedule = await schrepo.SearchSpecificSlotAsync(info.EmployeeID, date, schedule.Slot);
+        schedule = await schrepo.SearchSpecificSlotAsync(employeeID, date, schedule.Slot);
         if( schedule == null )
             return "Cannot get the schedule";
         if( schedule.SlotStatus == false )
             return "Cannot place order on this slot";
 
-        schedule.SlotCapacity = info.NumberOfFish;
         schedule.SlotStatus = false;
         schedule = await schrepo.UpdateSlotAsync(schedule);
         if( schedule == null )
@@ -61,40 +62,46 @@ public class BookingRepository : GenericRepository<Booking>
         return schedule.ScheduleID;
     }
 
-    public async Task<string> AddNewBooking(Booking info, string ServiceID){
+    public async Task<string> AddNewBooking(NewBookingInformation info ){
         var transaction = _context.Database.BeginTransactionAsync();
         string result = "";
+        Booking newOrder = new Booking();
         try{
-            if( info.CustomerID == null || info.EmployeeID == null || info.BookingAddress == null )
-                return result = "Parameters cannot be null";
-
-            if( info.CustomerID.Count() == 0 || info.EmployeeID.Count() == 0 ||
-                    info.NumberOfFish == 0 || info.BookingAddress.Count() == 0 )
-                return result = "Parameters cannot be empty";
-
-            if( info.BookingDate < DateTime.Now.AddDays(1) || info.BookingDate > DateTime.Now.AddDays(21) )
-                return result = "Cannot place an booking order with that date";
+            result = info.IsEmpty();
+            if( result != "Ok" )
+                return result;
 
             Customer? customer = await (new CustomerRepository(_context)).GetByIdAsync(info.CustomerID);
             if( customer == null )
                 return result = "Customer does not exist";
 
-            Employee employee = await (new EmployeeRepository(_context)).GetByIdAsync(info.EmployeeID);
+            Employee? employee = await (new EmployeeRepository(_context)).GetByIdAsync(info.EmployeeID);
             if( employee == null )
                 return result = "Employee does not exist";
 
-            Service service = await (new ServiceRepository(_context)).GetByIdAsync(ServiceID);
+            Service? service = await (new ServiceRepository(_context)).GetByIdAsync(info.ServiceID);
             if(service == null)
                 return result = "Cannot detemined the delivery method of the service";
-            info.ServiceDeliveryMethodID = service.ServiceDeliveryMethodID;
 
+            if( info.BookingDate < DateTime.Now.AddDays(2) || info.BookingDate >= DateTime.Now.AddDays(21) )
+                return "Cannot place an booking order with that date";
 
-            FeedbackRepository feedbackRepo = new FeedbackRepository(_context);
-            info.FeedbackID = (await feedbackRepo.SaveAndGetFeedbackAsync(new Feedback(){Status = "Uncommented"})).FeedbackID;
+            newOrder.CustomerID = info.CustomerID;
+            newOrder.EmployeeID = info.EmployeeID;
+            newOrder.BookingDate = info.BookingDate;
+            newOrder.ExpiredDate = info.BookingDate.AddHours(2);
+            newOrder.NumberOfFish = info.NumberOfFish;
+            newOrder.IncidentalFish = 0;
+            newOrder.ServiceDeliveryMethodID = info.SDM;
+            newOrder.BookingAddress = info.BookingAddress;
+            newOrder.Distance = 0;
+            newOrder.DistanceCost = 0;
+            newOrder.TotalServiceCost = 0;
+            newOrder.Status = Constants.Customer.WaitingForPayment;
+            newOrder.PaymentMethod = "Bank Transfer";
+            newOrder.PaymentStatus = "Pending";
 
-            info.IncidentalFish = 0;
-
-            string temp = await SetScheduleAsync(info);
+            string temp = await SetScheduleAsync(info.BookingDate, info.EmployeeID);
             switch (temp){
                 case "Outside working hour":
                 case "Cannot get the schedule":           
@@ -102,28 +109,25 @@ public class BookingRepository : GenericRepository<Booking>
                 case "Cannot update slot status":
                     return result = temp;
                 default:
-                    info.ScheduleID = temp;
+                    newOrder.ScheduleID = temp;
                     break;
             }
 
+            FeedbackRepository feedbackRepo = new FeedbackRepository(_context);
+            newOrder.FeedbackID = (await feedbackRepo.SaveAndGetFeedbackAsync(new Feedback(){Status = "Uncommented"})).FeedbackID;
+
             int index = base.GetAllAsync().Result.Count;
-            info.BookingID = "B" + index;
+            newOrder.BookingID = "B" + index;
 
-            TimeSpan remainingTime = (info.BookingDate - DateTime.Now) / 2;
-            info.ExpiredDate = DateTime.Now.Add(remainingTime);
-
-            info.Status = "Waiting for Payment";
-            info.PaymentStatus = "Pending";
-
-            if( await base.CreateAsync(info) == 0 )
+            if( await base.CreateAsync(newOrder) == 0 )
                 return result = "Unable to create new booking order";
 
-            BookingDetail detail = new BookingDetail(){BookingDetailID = "", BookingID = info.BookingID, ServiceID = ServiceID };
-            await (new BookingDetailRepository(_context)).AddBookingDetailAsync(detail, ServiceID); 
+            BookingDetail detail = new BookingDetail(){BookingDetailID = "", BookingID = newOrder.BookingID, ServiceID = info.ServiceID };
+            await (new BookingDetailRepository(_context)).AddBookingDetailAsync(detail, info.ServiceID); 
             await transaction.Result.CommitAsync();
-            return result = info.BookingID;
+            return result = newOrder.BookingID;
         } finally {
-            if( result != info.BookingID )
+            if( result != newOrder.BookingID )
                 await transaction.Result.RollbackAsync();
         }
     }
