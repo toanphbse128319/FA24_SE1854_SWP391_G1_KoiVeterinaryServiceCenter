62e8d0a4d94ee4233c1768ea883745911931a6b8
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-23 00:18:42 +0700

    Decouple Service dropdown and make API call to show on webpage

--- Source code/frontend/src/Components/Navbar.jsx
+++ Source code/frontend/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Dialog,
   DialogPanel,
@@ -27,42 +27,73 @@ import {
   PlayCircleIcon,
 } from "@heroicons/react/20/solid";
 
-const products = [
-  {
-    name: "Analytics",
-    description: "Get a better understanding of your traffic",
-    href: "#",
-    icon: ChartPieIcon,
-  },
-  {
-    name: "Engagement",
-    description: "Speak directly to your customers",
-    href: "#",
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: "Security",
-    description: "Your customers’ data will be safe and secure",
-    href: "#",
-    icon: FingerPrintIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools",
-    href: "#",
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: "Automations",
-    description: "Build strategic funnels that will convert",
-    href: "#",
-    icon: ArrowPathIcon,
-  },
-];
-const callsToAction = [
-  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  { name: "Contact sales", href: "#", icon: PhoneIcon },
-];
+import getAPIURL from "../Helper/Utilities";
+
+function GetService({ setServices }) {
+    let url = "/service";
+    url = getAPIURL( url );
+
+    useEffect(() => {
+        console.log("sending: " + url); 
+        fetch(url)
+            .then( response => response.json() )
+            .then( json => setServices( json ) )
+            .catch( error => console.error( error ) );
+        }, [] );
+}
+
+function ServiceDropdown(){
+    const [services, setServices] = useState([]);
+    GetService( { setServices: setServices } );
+    console.log( services );
+    
+    return (
+        <>
+            <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+              <Popover className="relative">
+                <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                  Service
+                  <ChevronDownIcon
+                    aria-hidden="true"
+                    className="h-5 w-5 flex-none text-gray-400" />
+                </PopoverButton>
+
+                <PopoverPanel
+                  transition
+                  className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in" >
+                  <div className="p-4">
+                    {services.map((service) => (
+                      <div
+                        key={service.name}
+                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50" >
+                        <div className="flex-auto">
+                          <a href="#" className="block font-semibold text-gray-900" >
+                            {service.name}
+                            <span className="absolute inset-0" />
+                          </a>
+                          <p className="mt-1 text-gray-600">{service.description}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </PopoverPanel>
+              </Popover>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Pet Care
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                News
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Contact
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                AboutUs
+              </a>
+            </PopoverGroup>
+        </>
+    );
+}
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -94,75 +125,7 @@ export default function Example() {
           </button>
         </div>
 
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <Popover className="relative">
-            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Service
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="h-5 w-5 flex-none text-gray-400"
-              />
-            </PopoverButton>
-
-            <PopoverPanel
-              transition
-              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-              <div className="p-4">
-                {products.map((item) => (
-                  <div
-                    key={item.name}
-                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                  >
-                    <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                      <item.icon
-                        aria-hidden="true"
-                        className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                      />
-                    </div>
-                    <div className="flex-auto">
-                      <a
-                        href={item.href}
-                        className="block font-semibold text-gray-900"
-                      >
-                        {item.name}
-                        <span className="absolute inset-0" />
-                      </a>
-                      <p className="mt-1 text-gray-600">{item.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                {callsToAction.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                  >
-                    <item.icon
-                      aria-hidden="true"
-                      className="h-5 w-5 flex-none text-gray-400"
-                    />
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </PopoverPanel>
-          </Popover>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Pet Care
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            News
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Contact
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            AboutUs
-          </a>
-        </PopoverGroup>
+      <ServiceDropdown />
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
