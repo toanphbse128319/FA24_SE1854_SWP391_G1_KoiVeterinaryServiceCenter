aaa3bc3fdd972812eab3b2851890b3c0eb268f04
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-25 15:37:22 +0700

    Added marker and change marker using click

--- Source code/frontend/src/Components/MapPicker.jsx
+++ Source code/frontend/src/Components/MapPicker.jsx
@@ -1,10 +1,10 @@
 import '@geoapify/geocoder-autocomplete/styles/round-borders.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import maplibregl from 'maplibre-gl';
 import './Styling/Map.css'
 
 import { GeoapifyContext, GeoapifyGeocoderAutocomplete } from '@geoapify/react-geocoder-autocomplete';
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 //function MapPicker( {mapIsReadyCallback} ){
 //    let address = "411 Nguyễn Văn Tăng, Long Thạnh Mỹ, Quận 9, Hồ Chí Minh, Vietnam";
@@ -48,30 +48,55 @@ import React, { useRef, useEffect } from "react";
 //
 
 export default function Map(){
+    let marker = useRef(null); 
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const lng = 106.84452525554275;
-    const lat = 10.846653659695292;
-    const zoom = 4;
+    let lng = useRef(106.84452525554275);
+    let lat = useRef(10.846653659695292);
+    const zoom = 18;
     const APIKey = "1e375620e6474ac2b7bd4eb2570e3091";
     const url = "https://maps.geoapify.com/v1/styles/osm-carto/style.json?";
     const style = "osm-bright";
 
     useEffect(() => {
-        if( map.current ) return; //Stop map from initialing twice
+        if( map.current ){
+            return; //Stop map from initialing twice
+        }
 
         map.current = new maplibregl.Map({
             container: mapContainer.current,
             style: `${url}style=${style}&apiKey=${APIKey}`,
-            center: [lng, lat],
+            center: [lng.current, lat.current],
             zoom: zoom
         });
-        
-    }, [APIKey]);
+
+        map.current.addControl( new maplibregl.NavigationControl() );
+        marker = new maplibregl.Marker({
+            color: "#FF0000",
+            draggable: false
+        }).setLngLat([lng.current, lat.current])
+            .addTo(map.current);
+        map.current.on('click', (e) => {
+            OnMapClick( e );
+        });
+    }, [lng, lat, zoom]);
+
+    function OnMapClick( e ){
+        console.log( marker );
+        if (marker) {
+            marker.remove();
+            lng.current = e.lngLat.lng;
+            lat.current = e.lngLat.lat;
+            marker.setLngLat([lng.current, lat.current]);
+            marker.addTo(map.current);
+        }
+        console.log('A click event has occurred at ' + e.lngLat);
+    }
 
     return (
         <div className="map-wrap">
-            <div ref={mapContainer} className="map" />
+        <div ref={mapContainer} className="map" />
         </div>
     );
 }
+
