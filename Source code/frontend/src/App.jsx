9bd767d9a251b4d2519d7c512c30496cbba8a666
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-27 16:18:58 +0700

    Temporary change the Fetch json data method to be universal

--- Source code/frontend/src/App.jsx
+++ Source code/frontend/src/App.jsx
@@ -7,13 +7,40 @@ import AboutUsFake from "./Components/AboutUsFake";
 import Doctor  from "./Components/DoctorLanding";
 import { Outlet } from "react-router-dom";
 import News from "./Components/News";
+import { useState, useEffect } from 'react';
+import { FetchJSON } from "./Helper/Utilities";
 
 
 const App = () => {
+    const [services, SetServices] = useState([]);
+    const [sdm, SetSDM] = useState([]);
+    const [loading, SetLoading] = useState(true);
+    useEffect(() => {
+        async function GetData(){
+            try {
+                // Set loading to true before fetching
+                SetLoading(true);
+                await FetchJSON({ endpoint: '/service', Setter: SetServices });
+                await FetchJSON({ endpoint: '/servicedeliverymethod', Setter: SetSDM });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                // Set loading to false after fetching
+                SetLoading(false);
+            }
+        }
+        GetData();
+    }, []);
+    if(loading){
+        return (
+            <div>Loading...</div>
+        );
+    }
+
   return (
     <div className="overflow-x-hidden">
-      <Navbar />
-      <Banner />
+      <Navbar services={services} SetServices={SetServices} sdm={sdm} SetSDM={SetSDM}/>
+      <Banner allServices={services} sdm={sdm}/>
       <NhanHang />
       <AboutUsFake />
       <Doctor/>
--- Source code/frontend/src/Helper/Utilities.jsx
+++ Source code/frontend/src/Helper/Utilities.jsx
@@ -3,6 +3,13 @@ function getAPIURL( endpoint ){
     return url;
 }
 
+export async function FetchJSON({ endpoint, Setter }) {
+    let url = getAPIURL(endpoint);
+    let response = await fetch(url).catch( (error) => console.error( error ) );
+    let json = await response.json();
+    Setter(json);
+}
+
 export function LogOut(){
     window.sessionStorage.clear();
 }
