e9bf7a3be5da9f07be9e75a47c8fdeac2e2950d5
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-10-30 13:52:29 +0700

    Recalibrate time

--- Source code/backend/Repositories/Repository/SlotTableRepository.cs
+++ Source code/backend/Repositories/Repository/SlotTableRepository.cs
@@ -29,7 +29,7 @@ public class SlotTableRepository : GenericRepository<SlotTable>
             case 8: return 2;
             case 9: return 3;
             case 10: return 4;
-            case 11: return 5;
+            case 13: return 5;
             case 14: return 6;
             case 15: return 7;
             case 16: return 8;
