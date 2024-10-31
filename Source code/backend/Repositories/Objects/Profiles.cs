31d504f4741f233a92cecca1ba9e53fc93c24cf0
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-10-31 16:49:50 +0700

    Added object to hold profiles

---
+++ Source code/backend/Repositories/Objects/Profiles.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+using Repositories.Model;
+
+namespace Repositories.Objects;
+
+public class Profiles
+{
+
+    public List<AnimalProfile> AnimalProfile { get; set; }
+    public List<PoolProfile> PoolProfile { get; set; }
+}
