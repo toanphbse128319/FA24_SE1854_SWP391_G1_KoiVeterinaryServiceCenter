e84c3d012eabe63264b4de2007ab1c9b1218a805
toanphbse128319 <toanphbse182319@fpt.edu.vn>
2024-11-01 17:19:17 +0700

    Added stopper if the appsetting.json is missing or not enoguh

--- Source code/backend/Helper/Mail.cs
+++ Source code/backend/Helper/Mail.cs
@@ -14,17 +14,23 @@ public class Mail
      */
     public Mail(string recepient)
     {
+        string host = Configuration.GetConfiguration()["Mail:Server"];
+        string port = Configuration.GetConfiguration()["Mail:Port"];
+        string mail = Configuration.GetConfiguration()["Mail:Email"];
+        string password = Configuration.GetConfiguration()["Mail:Password"];
+        if( host == null || port == null || mail == null || password == null )
+            throw new Exception("Missing configuration from appsetting.json");
         SmtpClient = new SmtpClient
         {
-            Host = Configuration.GetConfiguration()["Mail:Server"],
-            Port = int.Parse(Configuration.GetConfiguration()["Mail:Port"]),
+            Host = host,
+            Port = int.Parse(port),
             EnableSsl = true,
             DeliveryMethod = SmtpDeliveryMethod.Network,
             UseDefaultCredentials = false,
-            Credentials = new NetworkCredential(Configuration.GetConfiguration()["Mail:Email"], Configuration.GetConfiguration()["Mail:Password"])
+            Credentials = new NetworkCredential( mail, password )
         };
 
-        Email = new MailMessage(Configuration.GetConfiguration()["Mail:Email"], recepient);
+        Email = new MailMessage(mail, recepient);
     }
 
 
@@ -43,4 +49,4 @@ public class Mail
         Email.Subject = subject;
         Email.Body = message;
     }
-}
+ }
