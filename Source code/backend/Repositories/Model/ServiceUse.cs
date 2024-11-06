d7c67011f26e98d2b59a885353651adb9cb5a8a5
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-11-06 18:17:47 +0700

    Add nullable to properties

--- Source code/backend/Repositories/Model/ServiceUse.cs
+++ Source code/backend/Repositories/Model/ServiceUse.cs
@@ -13,8 +13,8 @@ namespace Repositories.Model
     public class ServiceUse
     {
         public string ServiceUseID { get; set; }
-        public string AnimalProfileID { get; set; }
-        public string PoolProfileID { get; set; }
-        public string BookingDetailID { get; set; }
+        public string? AnimalProfileID { get; set; }
+        public string? PoolProfileID { get; set; }
+        public string? BookingDetailID { get; set; }
     }
 }
