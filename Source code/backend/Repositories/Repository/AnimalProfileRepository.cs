90e23071feca10a34b38d0f2b956851ed9f409fa
quanvmse182416 <quanvmse182416@fpt.edu.vn>
2024-11-06 18:18:23 +0700

    Able to create serviceuse now

--- Source code/backend/Repositories/Repository/AnimalProfileRepository.cs
+++ Source code/backend/Repositories/Repository/AnimalProfileRepository.cs
@@ -55,6 +55,7 @@ namespace Repositories.Repository
             var transaction = await _context.Database.BeginTransactionAsync();
             bool rs = false;
             int count = 0;
+            ServiceUseRepository surepo;
             try
             {
 
@@ -64,6 +65,8 @@ namespace Repositories.Repository
                     item.AnimalProfileID = ap.GetNextID("AP");
                     if (await ap.CreateAsync(item) != 0)
                         count++;
+                    surepo = new ServiceUseRepository(_context);
+                    await surepo.CreateAsync(new ServiceUse() { ServiceUseID = surepo.GetNextID("SU") , AnimalProfileID = item.AnimalProfileID , BookingDetailID = exam.BookingDetail.BookingDetailID});
                 }
                 foreach (var item in exam.PoolProfile)
                 {
@@ -71,9 +74,12 @@ namespace Repositories.Repository
                     item.PoolProfileID = pp.GetNextID("PP");
                     if (await pp.CreateAsync(item) != 0)
                         count++;
+                    surepo = new ServiceUseRepository(_context);
+                    await surepo.CreateAsync(new ServiceUse() { ServiceUseID = surepo.GetNextID("SU"), AnimalProfileID = item.PoolProfileID, BookingDetailID = exam.BookingDetail.BookingDetailID });
                 }
                 if (count == (exam.AnimalProfile.Count + exam.PoolProfile.Count))
                     rs = true;
+
                 await transaction.CommitAsync();
                 return rs ? 1 : 0;
             }
